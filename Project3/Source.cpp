5e2fb6eb35a6d72fa94472b769a1eba9a385f09a
Denis Lion <levdenis01@gmail.com>
2023-03-14 00:36:26 +0700

    first commit

---
+++ Project3/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "enter 1 number  ";
+	cin >> a;
+	cout << "enter 2 number  ";
+	cin >> b;
+	if (a % 2 == 0 && b % 2 == 0) cout << "false";
+	else cout << "true";
+
+}
